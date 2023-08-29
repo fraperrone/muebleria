3374d66dd65ce1d46d764ef1860bd0604d7c5217
fraperrone <fperronerey@gmail.com>
2023-08-29 07:19:41 -0300

    primer commit - celular - estuctura - produccion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { MainComponent } from './main/main.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CarouselComponent,
+    MainComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
